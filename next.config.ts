b8fd6e92442e477de7ebdf4bffaa27400a82f165
AbdAlhalemEz <amezzal@lakeheadu.ca>
2026-02-21 07:21:03 -0500

    fix: explicit export path and trailing slash for gh-pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   // Change this to your repo name if not using a custom domain
   // basePath: process.env.NODE_ENV === 'production' ? '/portifolio' : '',
   images: {
